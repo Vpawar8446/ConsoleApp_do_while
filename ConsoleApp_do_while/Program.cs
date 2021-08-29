bf8769324f37e85e026f067a4221cec9f69f2ee7
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 23:32:59 +0530

    Add project files.

---
+++ ConsoleApp_do_while/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp_do_while
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("Element = {0}", i);
+                i++;
+            }
+            while (i <= 10);
+        }
+    }
+}
